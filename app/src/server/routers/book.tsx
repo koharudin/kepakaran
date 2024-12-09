74aa7c1c726237b8833893a856c44d855ca17b6a
Koharudin <koharudin.mail07@gmail.com>
2024-12-10 05:48:41 +0700

    add docs

--- app/src/server/routers/book.tsx
+++ app/src/server/routers/book.tsx
@@ -2,42 +2,25 @@ import { z } from "zod"
 import { publicProcedure, router } from "../trpc"
 
 export const bookRouter = router({
-  create: publicProcedure
-    .input(
-      z.object({
-        title: z.string(),
-        author: z.string(),
-        description: z.string(),
-      }),
-    )
-    .mutation(async ({ input, ctx }) => {
-      const data = await ctx.prisma.books.create({
-        data: {
-          title: input.title,
-          author: input.author,
-          description: input.description,
-        },
-      })
-      return { data }
-    }),
-  getz:publicProcedure.query(async({ input, ctx })=>{
+ 
+  getz: publicProcedure.query(async ({ input, ctx }) => {
     return 123
   }),
   get: publicProcedure
-    .input(
-      z.object({
-        isFavorite: z.boolean(),
-      }),
-    )
-    .query(async ({ input, ctx }) => {
-      return 
-      
+  
+  .input(
+    z.object({
+      isFavorite: z.boolean(),
+    })
+  )
+    .query(() => {
+      return `1234`
     }),
   delete: publicProcedure
     .input(
       z.object({
         bookId: z.string(),
-      }),
+      })
     )
     .mutation(async ({ input, ctx }) => {
       const data = await ctx.prisma.books.delete({
@@ -52,7 +35,7 @@ export const bookRouter = router({
       z.object({
         bookId: z.string(),
         isFavorite: z.boolean(),
-      }),
+      })
     )
     .mutation(async ({ input, ctx }) => {
       const data = await ctx.prisma.books.update({

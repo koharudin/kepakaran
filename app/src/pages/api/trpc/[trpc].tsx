d894e2778682cad0d71c392f8019d2c64597c7ac
unknown <koharudin.mail07@gmail.com>
2024-12-09 14:08:07 +0700

    add trpc

--- app/src/pages/api/trpc/[trpc].tsx
+++ app/src/pages/api/trpc/[trpc].tsx
@@ -2,6 +2,7 @@ import * as trpcNext from "@trpc/server/adapters/next";
 import { createContext } from "~/server/context";
 import { appRouter } from "~/server/routers";
 
+
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext,
---
+++ app/src/pages/api/trpc/cek.tsx
@@ -0,0 +1,4 @@
+
+export default(req, res)=>{
+    return res.status(200).send("cek bang...This is the correct way");
+}
